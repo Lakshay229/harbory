1a7a7ff92c92f14d12daa305ed8b1caea0cdd1eb
Lakshay229 <142925731+Lakshay229@users.noreply.github.com>
2025-06-29 16:39:26 +0530

    feat: integrate Tailwind CSS and set up routing
    - Added Tailwind CSS and its Vite plugin to the project.
    - Removed existing CSS styles and replaced with Tailwind CSS imports.
    - Implemented React Router for navigation between Home and Container pages.
    - Created a Navbar component for site navigation.
    - Developed Home and Container pages with basic layout and structure.
    - Updated package.json and package-lock.json to include new dependencies.

---
+++ harbory-frontend/src/pages/Container.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+const Container = () => {
+  return (
+    <div className='h-screen w-screen bg-gray-950 overflow-hidden m-0'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Container
